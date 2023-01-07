be5987b6b59bd422218465f0357d33810557fc67
Mateusz Izdebski <mati2k51@gmail.com>
2023-01-07 08:50:01 +0100

    List-of-Albums

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,33 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">MyMusicShop</a>
       </header>
-      <main>List albums</main>
+      <main>
+        <h1>Albums</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                  <p>{product.author}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
